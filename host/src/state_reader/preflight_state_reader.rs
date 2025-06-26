c7806a6f412fff10ea07d2851a3bff9f84ebf96f
eric tu <tu.eric@hotmail.com>
2025-06-26 17:12:32 +0000

    license

--- host/src/state_reader/preflight_state_reader.rs
+++ host/src/state_reader/preflight_state_reader.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use beacon_types::{ChainSpec, EthSpec};
 use ethereum_consensus::electra::{Validator, mainnet::VALIDATOR_REGISTRY_LIMIT};
 use ssz_multiproofs::MultiproofBuilder;
